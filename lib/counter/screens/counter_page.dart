64818e1304311af32dc870a66281368a0bdbc63f
AbhinandhanaPU <abhinandhanapu81@gmail.com>
2025-03-07 00:45:05 +0530

    solved --type_literal_in_constant_pattern-- issues on counterpage

--- lib/counter/screens/counter_page.dart
+++ lib/counter/screens/counter_page.dart
@@ -23,61 +23,56 @@ class _CounterPageState extends State<CounterPage> {
     return BlocBuilder<CounterBloc, CounterState>(
       bloc: counterBloc,
       builder: (context, state) {
-        switch (state.runtimeType) {
-          case CounterLoadingState:
-            return Scaffold(
-              body: Center(
-                child: CircularProgressIndicator(),
-              ),
-            );
-          case CounterInitialState:
-            final countState = state as CounterInitialState;
-            return Scaffold(
-              appBar: AppBar(
-                backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-                title: Text('Counter App'),
-              ),
-              body: Center(
-                  child: Column(
+        if (state is CounterLoadingState) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        } else if (state is CounterInitialState) {
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+              title: const Text('Counter App'),
+            ),
+            body: Center(
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Text(
-                    countState.count.toString(),
+                    state.count.toString(),
                     style: Theme.of(context).textTheme.headlineMedium,
                   ),
                 ],
-              )),
-              floatingActionButton: Column(
-                spacing: 5,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  FloatingActionButton(
-                    onPressed: () {
-                      counterBloc.add(CounterIncrementEvent());
-                    },
-                    tooltip: 'Increment',
-                    child: const Icon(Icons.add),
-                  ),
-                  FloatingActionButton(
-                    onPressed: () {
-                      counterBloc.add(CounterDecrementEvent());
-                    },
-                    tooltip: 'Decrement',
-                    child: const Icon(Icons.remove),
-                  ),
-                  FloatingActionButton(
-                    onPressed: () {
-                      counterBloc.add(CounterResetEvent());
-                    },
-                    tooltip: 'Reset',
-                    child: const Icon(Icons.refresh),
-                  ),
-                ],
               ),
-            );
-
-          default:
-            return SizedBox();
+            ),
+            floatingActionButton: Column(
+              spacing: 10,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                FloatingActionButton(
+                  onPressed: () =>
+                      context.read<CounterBloc>().add(CounterIncrementEvent()),
+                  tooltip: 'Increment',
+                  child: const Icon(Icons.add),
+                ),
+                FloatingActionButton(
+                  onPressed: () =>
+                      context.read<CounterBloc>().add(CounterDecrementEvent()),
+                  tooltip: 'Decrement',
+                  child: const Icon(Icons.remove),
+                ),
+                FloatingActionButton(
+                  onPressed: () =>
+                      context.read<CounterBloc>().add(CounterResetEvent()),
+                  tooltip: 'Reset',
+                  child: const Icon(Icons.refresh),
+                ),
+              ],
+            ),
+          );
+        } else {
+          return const Scaffold(
+            body: Center(child: Text('Unexpected state!')),
+          );
         }
       },
     );
